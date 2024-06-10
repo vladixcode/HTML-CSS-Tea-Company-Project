bcdb95cc5612921f67234f373ccc2dba034be5bd
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-06-10 18:10:54 +0200

    Footer section - HTML&CSS; JavaScript to dynamically get current year

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const navBtn = document.getElementById('nav-btn')
 const navbar = document.getElementById('navbar')
 const navClose = document.getElementById('nav-close')
+
 // show nav
-navBtn.addEventListener('click', () => {
-  navbar.classList.add('showNav')
-})
+// navBtn.addEventListener('click', () => {
+//   navbar.classList.add('showNav')
+// })
+
 // close nav
-navClose.addEventListener('click', () => {
-  navbar.classList.remove('showNav')
-})
+// navClose.addEventListener('click', () => {
+//   navbar.classList.remove('showNav')
+// })
+
 //setup date
-const date = (document.getElementById('date').innerHTML = new Date().getFullYear())
+document.getElementById('date').innerHTML = new Date().getFullYear()
