7427e9d81c30f40b193feec84be0d2848a4201df
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-06-14 12:09:14 +0200

    Navbar - HTML&CSS with position fixed and top left 0 with toggle functionality using JavaScript

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const navbar = document.getElementById('navbar')
 const navClose = document.getElementById('nav-close')
 
 // show nav
-// navBtn.addEventListener('click', () => {
-//   navbar.classList.add('showNav')
-// })
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav')
+})
 
 // close nav
-// navClose.addEventListener('click', () => {
-//   navbar.classList.remove('showNav')
-// })
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav')
+})
 
 //setup date
 document.getElementById('date').innerHTML = new Date().getFullYear()
